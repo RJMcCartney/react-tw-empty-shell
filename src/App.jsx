6045537a6fc2f0aaa6de97a103b0649c0fbdd8f1
Ryan <ryanmwork@hotmail.com>
2024-07-21 10:18:03 +0200

    feat(templates): update packages and add daisy

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,15 @@ function App() {
   return (
     <div class="text-3xl font-bold underline text-blue-500">
       TW Styling is working
+
+      Daisy Components working
+     <button class="btn">Button</button>
+    <button class="btn btn-neutral">Neutral</button>
+    <button class="btn btn-primary">Primary</button>
+    <button class="btn btn-secondary">Secondary</button>
+    <button class="btn btn-accent">Accent</button>
+    <button class="btn btn-ghost">Ghost</button>
+    <button class="btn btn-link">Link</button>
     </div>
   );
 }
